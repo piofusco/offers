1d7af1120ee49b67d9274174b85aa1cd05e0cf54
gideon <mpace@pivotal.io>
2018-01-31 23:31:10 -0700

    Initial commit!
    - Add base project
    - Add gitignore
    - Remove Main Storyboard

---
+++ Offers/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Offers
+//
+//  Created by gideon on 1/31/18.
+//  Copyright © 2018 piofusco. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        let homeViewController = UIViewController()
+        homeViewController.view.backgroundColor = UIColor.blue
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window!.rootViewController = homeViewController
+        window!.makeKeyAndVisible()
+
+        return true
+    }
+}
---
+++ Offers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Offers
+//
+//  Created by gideon on 1/31/18.
+//  Copyright © 2018 piofusco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
